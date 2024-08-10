8a352c66cd58109b4602d97cc67d699e73a45afe
Shadh-muhammed <shadhbnc@gmail.com>
2024-08-10 13:48:21 +0530

    data

--- src/exception.py
+++ src/exception.py
@@ -1,20 +1,30 @@
 import sys
-from src.logger import logging
-
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = (
-        f"Error occurred in python script name [{file_name}] "
-        f"line number [{exc_tb.tb_lineno}] error message [{error}]"
-    )
-    return error_message
+import traceback
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    def __init__(self, error):
+        super().__init__(str(error))
+        self.error_message = self.get_error_message(error)
+    
+    def get_error_message(self, error):
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+        error_message = (
+            f"Error occurred in python script: {file_name} "
+            f"at line {line_number} - {type(error).__name__}: {error}"
+        )
+        return error_message
     
     def __str__(self):
         return self.error_message
+
+# Example usage
+try:
+    # Some code that might raise an exception
+    1 / 0
+except Exception as e:
+    raise CustomException(e)
+
+
 
